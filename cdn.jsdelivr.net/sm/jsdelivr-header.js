514457bb20882017b514adb7205802843e3ed63e
luisbossa <luisbossa@gmail.com>
2025-11-23 22:02:56 -0600

    web reorder

--- cdn.jsdelivr.net/sm/jsdelivr-header.js
+++
@@ -1,6 +0,0 @@
-/**
- * Minified by jsDelivr using Terser v5.37.0.
- * Original file: /gh/the-lazy-god/tlg-count@1.0.0/tlg-count.js
- *
- * Do NOT use SRI with dynamically generated files! More information: https://www.jsdelivr.com/using-sri-with-dynamic-files
- */
